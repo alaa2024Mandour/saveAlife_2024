837e28c225d77d9bb553fc306471a2b43de9b2f5
alaaMandour <alaaMandour2024@gmail.com>
2024-03-16 00:25:42 +0200

    edit splash screen And finish Stuff_User Screen

--- lib/shared/style/colors.dart
+++ lib/shared/style/colors.dart
@@ -3,3 +3,6 @@ import 'package:hexcolor/hexcolor.dart';
 
 String hexColor = "#E01B18";
 Color defultColor = Color(int.parse(hexColor.substring(1, 7), radix: 16) + 0xFF000000);
+
+String text_hexColor = "#335B66";
+Color Text_Color = Color(int.parse(text_hexColor.substring(1, 7), radix: 16) + 0xFF000000);
