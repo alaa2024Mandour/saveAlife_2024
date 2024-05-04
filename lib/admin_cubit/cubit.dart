bab623247419c5a2dd933138aac1b747244a58ea
alaaMandour <alaaMandour2024@gmail.com>
2024-05-04 22:19:15 +0300

    PopUp for admin send donation result

---
+++ lib/admin_cubit/cubit.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:save_a_life_2024/admin_cubit/status.dart';
+
+class AdminCubit extends Cubit<AdminStatus> {
+  AdminCubit() : super(InitialAdminState());
+
+  static AdminCubit get(context) => BlocProvider.of(context);
+
+  // -------------- AdminController ----------------
+  // -------------- Upload files controller ----------------
+  var donorEmail = TextEditingController();
+  var FileController = TextEditingController();
+
+
+  String ?fileName;
+  Future uploadFile () async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if(result != null){
+      File file= File(result.files.single.path ?? " ");
+
+      fileName =file!.path.split('/').last;
+      String filePath = file!.path;
+
+      onSendProgress:(int send, int total){
+        print("$send , $total");
+      };
+      emit(UploadResult());
+    }else{
+      print("Result is null");
+    }
+
+  }
+
+}
